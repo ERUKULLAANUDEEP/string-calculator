acb0590a897abc8ef98ebe2bb730d955e133ed28
ERUKULLA ANUDEEP <erukulaanudeep63@gmail.com>
2025-02-12 17:31:21 +0530

    added implementation for comma separated numbered string testcases

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const add = (inputString) => {
   if(inputString === '') {
     return 0;
-  } else {
-    return parseInt(inputString); // This will also handle invalid input string
   }
+  const arr = inputString.split(',');
+  const numberedArr = arr.map((num) => {
+    return parseInt(num);
+  });
+  return numberedArr.reduce((acc, cur) => acc + cur, 0);
+   
 }
 module.exports = add;
