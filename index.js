e96c2d2a3601d233e0ac3a3910a67b901090202c
ERUKULLA ANUDEEP <erukulaanudeep63@gmail.com>
2025-02-12 19:00:52 +0530

    added implementation for handling custom delimiters  n length

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+function extracteDelimiter(delimiterPart) {
+    if (delimiterPart.startsWith("[") && delimiterPart.endsWith("]")) {
+        const extractedDelimiter = delimiterPart.match(/\[([^\]]+)\]/g).map((d) => d.slice(1, -1));
+        return new RegExp(
+            extractedDelimiter.map((d) => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")).join("|"))
+
+    } else {
+        return new RegExp(delimiterPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
+    }
+}
 const add = (inputString) => {
   if(inputString === '') {
     return 0;
   }
-  const arr = inputString.split(/[,\n]/);
+  let delimiter = /[,\n]/;
+  const hasCustomDelimiter = inputString.startsWith("//");
+  if (hasCustomDelimiter) {
+      const parts = inputString.split("\n");
+      const delimiterPart = parts[0].slice(2);
+      delimiter = extracteDelimiter(delimiterPart);
+      inputString = parts[1];
+  }
+
+  const arr = inputString.split(delimiter);
   const numberedArr = arr.map((num) => {
     return parseInt(num);
   });
