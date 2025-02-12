9069c0ed6607f53cfcdcb962688ff781cbc05e5a
ERUKULLA ANUDEEP <erukulaanudeep63@gmail.com>
2025-02-12 17:24:25 +0530

    added failing testcase for single number and invalid string

--- index.test.js
+++ index.test.js
@@ -2,8 +2,11 @@ const add = require('./index');
 describe('string-calculator', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).toBe(0);
-    })
+    });
     it('should return the same number for a single number string', () =>{
         expect(add('9')).toBe(9);
-    })
+    });
+    it('should return NAN for invalid string with a single number', () => {
+        expect(add('a')).toBe(NaN);
+    });
 });
