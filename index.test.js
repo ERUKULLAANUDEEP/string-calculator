dab9613ec8c5e35c77f101081a7fb96c24173efd
ERUKULLA ANUDEEP <erukulaanudeep63@gmail.com>
2025-02-12 17:20:40 +0530

    added failing testcase for single number string

--- index.test.js
+++ index.test.js
@@ -3,4 +3,7 @@ describe('string-calculator', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).toBe(0);
     })
+    it('should return the same number for a single number string', () =>{
+        expect(add('9')).toBe(9);
+    })
 });
